75e41648aca2105dd840321a9bee1ce4a86c288c
wuxx <wuxiaoxiang@wangtiansoft.com>
2020-04-08 18:39:41 +0800

    加入一堆

--- 插入排序.js
+++ 插入排序.js
@@ -3,9 +3,15 @@ const arr = [1, 33, 4, 22, 5, 19]
 
 !function main () {
     const len = arr.length
-    let preIndex = 0, curent = arr[0]
+    let preIndex, current
     for(let i = 1; i < len; i++ ) {
-
+        preIndex = i - 1
+        current = arr[i]
+        while (preIndex > 0 && arr[preIndex] > current) {
+            arr[preIndex + 1] = arr[preIndex]
+            --preIndex
+        }
+        arr[preIndex + 1] = current
     }
 }()
 
