a11acd7dadb803840c2cdd48ba0c84934ba91857
wuxx <wuxiaoxiang@wangtiansoft.com>
2020-04-20 18:33:25 +0800

    加入一堆

--- 复习.js
+++ 复习.js
@@ -2,14 +2,14 @@ const arr = [1, 33, 4, 22, 5, 19]
 
 !function main () {
     const len = arr.length
-    for (let i = 0; i < len; i++) {
+    for (let i = 0; i < len - 1; i++) {
         let minIndex = i
-        for (let j = i; j < len; j++ ) {
+        for (let j = i + 1; j < len; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j
             }
         }
-        [arr[minIndex], arr[i]] = [arr[i], arr[minIndex]]
+        [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]]
     }
 }()
 
